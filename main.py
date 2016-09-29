fbfa194fee5f9d9fb10eef4e76f27751164c16af
Victor Elizalde <vic-bull@hotmail.com>
2016-09-29 14:31:52 -0500

    Modifies conflicts

--- main.py
+++ main.py
@@ -24,10 +24,4 @@ cargarmaquina(idtrabajos, trabajos, maquinas)
 #Aplicar heuristica greedy para orden de operaciones en cada maquina
 depgreedy(idmaquinas, maquinas)
 
-<<<<<<< HEAD
 getvecinos(1, vecinos, idmaquinas, idtrabajos, maquinas, trabajos)
-=======
-printmaqop(idmaquinas, maquinas)
-
-print getmakespan(idmaquinas, maquinas)
->>>>>>> 1db770feaee2335f7c3082272847866ea4732c91
